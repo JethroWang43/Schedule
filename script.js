5c94a7423c6ea2dc488cd2f48b33c7631bdf502e
JethroWang24 <202311220@fit.edu.ph>
2026-01-04 00:49:37 +0800

    feat: add meeting links to course schedule for better accessibility

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ window.dummyData = {
     { course: "IT0039", time: "01:00 PM - 02:50 PM", subject: "IT PROJECT MANAGEMENT", room: "FTIC Project Room (CCS)" }
   ],
   Tuesday: [
-    { course: "IT0051", time: "07:00 AM - 08:50 AM", subject: "IT ELECTIVE - HUMAN COMPUTER INTERACTION 2", room: "ONLINE" },
-    { course: "IT0039", time: "09:00 AM - 10:50 AM", subject: "IT PROJECT MANAGEMENT", room: "ONLINE" },
+    { course: "IT0051", time: "07:00 AM - 08:50 AM", subject: "IT ELECTIVE - HUMAN COMPUTER INTERACTION 2", room: "ONLINE", link: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTgyMjI2ODItNmFkNy00NjFlLWIyOGUtYjdmZjQ5MzE4Yjk4%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522b0a025d9-cb88-4408-9b15-ce77d47c3810%2522%252c%2522Oid%2522%253a%252252e0919b-1da9-480a-a5a0-338b614d9a27%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=3ced33dc-df0d-4079-840e-9b6883064728&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true" },
+    { course: "IT0039", time: "09:00 AM - 10:50 AM", subject: "IT PROJECT MANAGEMENT", room: "ONLINE", link: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_YzVmZGI5MWUtZDY4ZC00MDQ4LWFjNDctMzNhZmE0OTk3Zjhh%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522b0a025d9-cb88-4408-9b15-ce77d47c3810%2522%252c%2522Oid%2522%253a%2522bd52fd16-477a-4067-a55f-8facc47df93c%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=e544bccb-581d-46ed-9db5-d3493999c4f8&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true" },
     { course: "IT0041", time: "01:00 PM - 02:50 PM", subject: "E-COMMERCE WITH DIGITAL MARKETING", room: "ONLINE" }
   ],
   Wednesday: [
@@ -19,7 +19,7 @@ window.dummyData = {
   ],
   Friday: [
     { course: "IT0023", time: "09:00 AM - 10:50 AM", subject: "SYSTEM INTEGRATION AND ARCHITECTURE 1", room: "ONLINE" },
-    { course: "IT0123", time: "11:00 AM - 12:50 PM", subject: "IT SPECIALISATION 7 - DEVELOPMENT NETWORK", room: "ONLINE" }
+    { course: "IT0123", time: "11:00 AM - 12:50 PM", subject: "IT SPECIALISATION 7 - DEVELOPMENT NETWORK", room: "ONLINE", link: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_MTUzOTliNzctZjczNi00OTQ4LThhM2QtNDdmNWJhMTQzYTBm%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522b0a025d9-cb88-4408-9b15-ce77d47c3810%2522%252c%2522Oid%2522%253a%252262410d1a-8ed9-4c2d-9727-f1801931cd5f%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=dde24a37-4a48-46df-889d-d1837ea9ecdc&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true" }
   ],
   Saturday: []
 };
